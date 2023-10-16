a689582f6c7027f55ed3a65fac8ff2a579c64535
Aleksander Bondar <86482559+AleksanderBondar@users.noreply.github.com>
2023-10-16 08:59:30 +0200

    :tada: Starters

--- packages/cli/create/index.ts
+++ packages/cli/create/index.ts
@@ -1,103 +1,38 @@
-import { execSync } from 'child_process';
 import { Command } from 'commander';
 import chalk from 'chalk';
-import fs from 'fs';
 
-import { BASE_REPOSITORY, STARTER_DICT, STARTERS } from './consts.js';
-import { correctPath, isPathValid, log, toUpperCase } from './utils.js';
+import { STARTER_DICT, STARTERS } from './consts.js';
+import { createAppAction } from './utils.js';
 
 export const createApp = (program: Command) => {
-  Object.entries(STARTER_DICT).forEach(([key, { description, repo, example }]) => {
+  Object.entries(STARTER_DICT).forEach(([key, { description: _description, repo, example }]) => {
     const starter = key as STARTERS;
+    const command = `create-${starter} [dir] [name]`;
+    const description = `${chalk.magenta('Axolotl Starter')} - ${_description}`;
+
     program
-      .command(`create-${starter} [dir] [name]`)
-      .description(
-        `${chalk.magenta('Axolotl Starter')} - ${description} - directory [dir] and name [name] are optional`,
-      )
-      .action((_destination, _name) => {
-        const destination = (_destination || './') as string;
-        const name = (_name || repo) as string;
-        const path = correctPath(destination, name);
-
-        const installationMessages = [
-          {
-            message: `Installing starter ${chalk.magenta('GraphQL Axolotl Server')} - GraphQL ${toUpperCase(
-              starter,
-            )} example...`,
-          },
-          { message: `This may take a while...`, color: 'white' as const },
-        ];
-        log(installationMessages);
-
-        if (invokeCommands(path, example, _name !== undefined)) {
-          const successMessages = [
-            {
-              message: `Starter ${chalk.magenta('GraphQL Axolotl Server')} - GraphQL ${toUpperCase(
-                starter,
-              )} example created.`,
-            },
-            { message: `To run it, type:`, color: 'white' as const },
-            {
-              message: `cd ${chalk.magenta(`${repo}`)} && ${chalk.magenta(`npm run start`)}`,
-              color: 'yellow' as const,
-            },
-          ];
-
-          log(successMessages);
-        } else {
-          const errorMessages = [
-            { message: `Starter ${starter} failed.`, color: 'red' as const },
-            { message: `Please try again.`, color: 'red' as const },
-          ];
-
-          log(errorMessages);
-        }
+      .command(command)
+      .description(description)
+      .action((destination, name) => createAppAction({ starter, repo, example, destination, name }))
+      .on('--help', () => {
+        console.log('');
+        console.log(`  Examples:`);
+        console.log('');
+        console.log(
+          `  First argument is the ${chalk.red('destination')} folder, second argument is the project ${chalk.red(
+            'name',
+          )}.`,
+        );
+        console.log('');
+        console.log(
+          `    $ axolotl create-${starter} my-graphql-server - ${chalk.gray('New project in specific destination')}`,
+        );
+        console.log(
+          `    $ axolotl create-${starter} my-graphql-server my-server - ${chalk.gray(
+            'New project with specific destination and name',
+          )}`,
+        );
+        console.log('');
       });
   });
 };
-
-function invokeCommands(path: string, example: string, different: boolean) {
-  try {
-    if (!isPathValid(path)) {
-      log([{ message: `Path ${path} is not valid.`, color: 'red' as const }]);
-      return false;
-    }
-
-    const commands = [
-      `git clone -n --depth=1 --filter=tree:0 ${BASE_REPOSITORY} ${path}`,
-      `cd ${path} && git sparse-checkout set --include=examples/${example} && git checkout`,
-    ];
-    commands.forEach((command) => !runCommand(command) && process.exit(1));
-
-    // NOT SURE ABOUT THIS PART
-    const json = JSON.parse(fs.readFileSync(`${path}/package.json`, 'utf8'));
-    const modifiedJson = {
-      ...json,
-      name: different ? path.split('/').pop() : json.name,
-    };
-    const updated = JSON.stringify(modifiedJson, null, 2);
-    if (updated !== json) fs.writeFileSync(`${path}/package.json`, updated, 'utf8');
-    // NOT SURE ABOUT THIS PART
-
-    const install = runCommand(`cd ${path} && npm install`);
-    if (!install) process.exit(1);
-
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
-
-function runCommand(command: string) {
-  try {
-    execSync(command, { stdio: 'inherit' });
-  } catch (e) {
-    console.error(e);
-    log([
-      { message: `Command ${command} failed.`, color: 'red' as const },
-      { message: `Please try again.`, color: 'white' as const },
-    ]);
-    return false;
-  }
-  return true;
-}
