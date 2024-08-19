fb1ff7884a27de0cef7b30249fb3d99da1e8f4c3
DTomArt <dtomart@gmail.com>
2024-08-19 16:38:06 +0200

    TeamMemberQuery.showTeamInvitations status filter

--- modularium/root/src/modules/teams/resolvers/TeamMemberQuery/TeamMemberQuery.ts
+++ modularium/root/src/modules/teams/resolvers/TeamMemberQuery/TeamMemberQuery.ts
@@ -1,15 +1,23 @@
 import { createResolvers } from '@/src/modules/teams/axolotl.js';
 import { TeamModule } from '@/src/modules/teams/functions/teams.js';
 import { MemberSource } from '@/src/modules/teams/orm.js';
+import { Filter } from 'mongodb';
+import { InviteToken } from '../../models.js';
 export default createResolvers({
   TeamMemberQuery: {
     showInviteTokens: async ([source]) => {
       const src = source as MemberSource;
       return TeamModule.showInviteTokens(src.team._id);
     },
-    showTeamInvitations: async ([source]) => {
+    showTeamInvitations: async ([source], { status }) => {
       const src = source as MemberSource;
-      return TeamModule.teamInvitationsByTeam({ teamId: src.team._id });
+      const query: Filter<InviteToken> = {};
+
+      if (status) {
+        query.status = { $eq: status };
+      }
+
+      return TeamModule.teamInvitationsByTeam({ ...query, teamId: src.team._id });
     },
     team: async ([source]) => {
       const src = source as MemberSource;
