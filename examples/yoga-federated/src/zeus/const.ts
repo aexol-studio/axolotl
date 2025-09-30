2e4a51179c100a11362579a76fee1be09ad0d483
aexol <aexol@aexol.com>
2025-09-30 09:51:14 +0200

    new versions

---
+++ examples/yoga-federated/src/zeus/const.ts
@@ -0,0 +1,68 @@
+/* eslint-disable */
+
+export const AllTypesProps: Record<string,any> = {
+	AuthorizedUserMutation:{
+		createTodo:{
+
+		},
+		todoOps:{
+
+		},
+		changePassword:{
+
+		}
+	},
+	AuthorizedUserQuery:{
+		todo:{
+
+		}
+	},
+	Mutation:{
+		login:{
+
+		},
+		register:{
+
+		}
+	},
+	ID: `scalar.ID` as const
+}
+
+export const ReturnTypes: Record<string,any> = {
+	Todo:{
+		_id:"String",
+		content:"String",
+		done:"Boolean"
+	},
+	TodoOps:{
+		markDone:"Boolean"
+	},
+	User:{
+		_id:"String",
+		username:"String"
+	},
+	AuthorizedUserMutation:{
+		createTodo:"String",
+		todoOps:"TodoOps",
+		changePassword:"Boolean"
+	},
+	AuthorizedUserQuery:{
+		todos:"Todo",
+		todo:"Todo",
+		me:"User"
+	},
+	Mutation:{
+		user:"AuthorizedUserMutation",
+		login:"String",
+		register:"String"
+	},
+	Query:{
+		user:"AuthorizedUserQuery"
+	},
+	ID: `scalar.ID` as const
+}
+
+export const Ops = {
+query: "Query" as const,
+	mutation: "Mutation" as const
+}
